36c774790696091f9b390014bd11d6d61c3c35d2
TERP <terposterp@yandex.com>
2019-11-29 20:27:33 -0500

    11/29/2019 Update
    Added global.h

---
+++ Birday_arcade/Birday_arcade/src/global.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_color.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_acodec.h>
+#include <vector>
+#include <iostream>
+#include <random>
+#include <time.h>
+#include <math.h>
+
+
+
+// screen size
+#define winx 1360
+#define winy 768
+
